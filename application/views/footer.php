d020380b2993223427542ea15004073eb6f7fa42
IbnHax0r <135268606+Ibnsyaebatul@users.noreply.github.com>
2025-09-10 09:24:21 +0700

    Create footer.php

---
+++ application/views/footer.php
@@ -0,0 +1,7 @@
+</div>
+  <footer class="container" style="text-align:center;background:transparent">
+    <small>&copy; <?= date('Y') ?> Project Diabict — CodeIgniter 3</small>
+  </footer>
+</div>
+</body>
+</html>
