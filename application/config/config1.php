3f224d8fa2332760db9fbe3ba4c12ae598403077
IbnHax0r <135268606+Ibnsyaebatul@users.noreply.github.com>
2025-09-10 09:10:52 +0700

    Create config1.php

---
+++ application/config/config1.php
@@ -0,0 +1,4 @@
+$config['base_url']   = 'http://localhost/diabict/';
+$config['index_page'] = '';                 // kalau pakai .htaccess
+$config['csrf_protection'] = TRUE;          // aktifkan CSRF
+$config['encryption_key'] = 'CHANGE_ME_32CHARS_MINIMAL'; // isi string acak ≥32 char
