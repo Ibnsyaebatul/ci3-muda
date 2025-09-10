ba79a9e017eac4fc591c6b9fdb35729b43f168dc
IbnHax0r <135268606+Ibnsyaebatul@users.noreply.github.com>
2025-09-10 09:26:01 +0700

    Update siswa_model.php

--- application/models/siswa_model.php
+++ application/models/siswa_model.php
@@ -4,9 +4,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Siswa_model extends CI_Model
 {
     private $table = 'tabel_siswa';
+    private $pk    = 'id';
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+    }
 
     public function all() {
-        return $this->db->order_by('id','DESC')->get($this->table)->result();
+        return $this->db->order_by($this->pk,'DESC')->get($this->table)->result();
     }
 
     public function count_all() {
@@ -14,7 +21,7 @@ class Siswa_model extends CI_Model
     }
 
     public function find($id) {
-        return $this->db->get_where($this->table, ['id'=>$id])->row();
+        return $this->db->get_where($this->table, [$this->pk => $id])->row();
     }
 
     public function create($data) {
@@ -22,10 +29,17 @@ class Siswa_model extends CI_Model
     }
 
     public function update($id,$data) {
-        return $this->db->where('id',$id)->update($this->table,$data);
+        return $this->db->where($this->pk,$id)->update($this->table,$data);
     }
 
     public function delete($id) {
-        return $this->db->delete($this->table,['id'=>$id]);
+        return $this->db->delete($this->table, [$this->pk=>$id]);
+    }
+
+    // cek NIS unik (kecuali id tertentu)
+    public function nis_exists($nis, $ignore_id = null) {
+        $this->db->where('nis', $nis);
+        if ($ignore_id) $this->db->where($this->pk.' !=', $ignore_id);
+        return $this->db->count_all_results($this->table) > 0;
     }
 }
